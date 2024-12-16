823ca958e61da6372afeb61ef6c9d16f58cdcdf0
Luca1882 <luca.notarfranco@gmail.com>
2024-12-16 01:21:58 +0100

    Feat: Aggiunto valori e condizionali, estrazione dati Total

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 //Richiamo evento "Submit"
-document.getElementById('ticket-form').addEventListener('submit'), function (call) {
-    call.preventDefault();
-}
+document.getElementById('ticket-form').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-//Estraggo i dati di inputKm e agePassenger
-const kilometers = parseFloat(document.getElementById('inputKm').value);
-const age = parseInt(document.getElementById('agePasseenger').value);
+
+    //Estraggo i dati di inputKm e agePassenger
+    const kilometers = parseFloat(document.getElementById('inputKm').value);
+    const age = parseInt(document.getElementById('agePassenger').value);
+
+    //Prezzo base
+    const priceKm = 0.21;
+    let totPrice = kilometers * priceKm;
+
+    //Applica sconti
+    if (age < 18) {
+        totPrice *= 0.8; //Sconto 20%
+    } else if (age >= 65) {
+        totPrice *= 0.6; //Sconto 40%
+    }
+
+    //Formattazione a due decimali
+    totPrice = totPrice.toFixed(2);
+    
+    //Estraggo i dati del totale
+    const tot = document.getElementById('total');
+    tot.innerHTML = `Prezzo totale: €${totPrice}`;
+});
+
+
 
 
